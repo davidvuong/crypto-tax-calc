577dd88e597cb7e2fcbd00cce552e1289810febb
Aaron Lee <zl.aaron.lee@gmail.com>
2022-06-04 23:54:47 +1000

    feat: add validate for decimaljs schema validator

--- src/validator.ts
+++ src/validator.ts
@@ -1,28 +1,40 @@
 import Decimal from 'decimal.js';
-import BaseJoi, { AnySchema, CoerceResult, Extension, Schema } from 'joi';
-import { join } from 'lodash';
+import BaseJoi, {
+  AnySchema,
+  CoerceResult,
+  CustomHelpers,
+  Extension,
+  Schema,
+} from 'joi';
+
+const name = 'bigdecimal';
 
 enum DecimalSchemaErrorCode {
   NUMBER = 'bigdecimal.number',
+  NOT_DECIMAL = 'bigdecimal.notdecimal',
 }
 
 enum DecimalSchemaFlag {}
 
 interface DecimalSchema extends AnySchema {}
 
-export const Joi: BaseJoi.Root & { bigdecimal(): DecimalSchema } =
-  BaseJoi.extend((joi): Extension => {
+// TODO: extract this to its own package
+export const Joi: BaseJoi.Root & { [name](): DecimalSchema } = BaseJoi.extend(
+  (joi): Extension => {
+    const messages: Record<DecimalSchemaErrorCode, string> = {
+      [DecimalSchemaErrorCode.NUMBER]: '{{#label}} must be a big number',
+      [DecimalSchemaErrorCode.NOT_DECIMAL]:
+        'key {{#label}} {{#value}} is not a decimal. Is convert enabled?',
+    };
+
     return {
-      type: 'bigdecimal',
+      type: name,
       base: joi.any(),
-      messages: {
-        [DecimalSchemaErrorCode.NUMBER]: '{{#label}} must be a big number',
-      },
+      messages,
       coerce(value, helpers): CoerceResult {
         // TODO: Error mapping following joi conventions
         // TODO: add extra validation rules
         const result = new Decimal(value);
-
         if (result.isNaN()) {
           return helpers.error(DecimalSchemaErrorCode.NUMBER);
         }
@@ -31,13 +43,20 @@ export const Joi: BaseJoi.Root & { bigdecimal(): DecimalSchema } =
           value: new Decimal(value),
         };
       },
+      validate(value: any, helpers: CustomHelpers) {
+        if (!(value instanceof Decimal)) {
+          return { errors: helpers.error(DecimalSchemaErrorCode.NOT_DECIMAL) };
+        }
+        return { value };
+      },
     };
-  });
+  }
+);
 
 export default Joi;
 
 export const validateSchema = <T>(data: unknown, schema: Schema): T => {
-  const { value, error } = schema.validate(data);
+  const { value, error } = schema.validate(data, { convert: false });
   if (error) {
     throw error;
   }
